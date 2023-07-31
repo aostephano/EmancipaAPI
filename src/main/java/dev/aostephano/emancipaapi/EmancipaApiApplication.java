67f875ba3dc2bd635d955d1d62299005fd1b45ad
Alexandre Stephano <ale.stephano@gmail.com>
2023-07-31 14:01:52 -0300

    Initial commit

---
+++ src/main/java/dev/aostephano/emancipaapi/EmancipaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.aostephano.emancipaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmancipaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmancipaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aostephano/emancipaapi/EmancipaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aostephano.emancipaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmancipaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
